f51121ee4ecb515b0fc117085064671f4016b1e0
Renato de Figueiredo <renato.fn10@gmail.com>
2022-08-13 22:45:50 -0300

    update

--- src/components/SocialProof.jsx
+++ src/components/SocialProof.jsx
@@ -1,11 +1,20 @@
 import "../styles/social-proof.scss"
 
+import depoimento01 from "../assets/depoimento1.jpg"
+import depoimento02 from "../assets/depoimento2.jpg"
+
 
 export function SocialProof() {
     return (
         <section id="social-proof">
             <div className="container">                    
                 <h2>Depoimentos de quem comprou</h2>
+                <div className="wrapper">
+                    <img src={depoimento01} alt="" />
+                    <img src={depoimento02} alt="" />
+                    <img src={depoimento01} alt="" />
+                    <img src={depoimento02} alt="" />
+                </div>
             </div>
         </section>
     )
